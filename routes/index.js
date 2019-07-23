c4fecd29197919f659c78e2da64280b8ab17e4f5
Rafael Oliveira <rafaelntn766@gmail.com>
2019-07-23 12:54:56 -0300

    Fisishing site and starting admin system

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservation');
+var contacts = require('./../inc/contacts');
 var router = express.Router();
 
 /* GET home page. */
@@ -10,40 +12,99 @@ router.get('/', function (req, res, next) {
 
     res.render('index', {
       title: 'Restaurante saboroso',
-      menus: results
+      menus: results,
+      isHome: true
     });
 
   });
 
-router.get('/contacts', function(req, res, next){
-  res.render('contact', {
-    title: 'Contato - Restaurante Saboroso',
-    background: 'images/img_bg_3.jpg',
-    h1: 'Diga um Oi!'
-  });
-});
+  router.get('/contacts', function (req, res, next) {
 
-router.get('/menus', function(req, res, next){
-  res.render('menu', {
-    title: "Menu - Restaurante Saboroso",
-    background: 'images/img_bg_1.jpg',
-    h1: 'Saboreie nosso menu!'
-  });
-});
+    contacts.render(req, res);
 
-router.get('/reservations', function(req, res, next){
-  res.render('reservation', {
-    title: "Reserva - Restaurante Saboroso",
-    background: 'images/img_bg_2.jpg',
-    h1: 'Reserve uma mesa!'
   });
-});
 
-router.get('/services', function(req, res, next){
-  res.render('services', {
-    title: "Serviços - Restaurante Saboroso",
-    background: 'images/img_bg_1.jpg',
-    h1: 'É um prazer poder servir!'
+  router.post('/contacts', function (req, res, next) {
+    
+    if(!req.body.name){
+      contacts.render(req, res, 'Digite o nome');
+    } else if(!req.body.email) {
+      contacts.render(req, res, 'Digite o e-mail');
+    } else if(!req.body.message) {
+      contacts.render(req, res, 'Digite a mensagem');
+    } else {
+
+      contacts.save(req.body).then(results => {
+
+        req.body = {};
+
+        contacts.render(req, res, null, "Contato enviado com sucesso!")
+
+      }).catch(err=>{
+
+        contacts.render(req, res, err.message);
+
+      })
+
+    }
+
+  });
+
+  router.get('/menus', function (req, res, next) {
+
+    menus.getMenus().then(results => {
+
+      res.render('menu', {
+        title: "Menu - Restaurante Saboroso",
+        background: 'images/img_bg_1.jpg',
+        h1: 'Saboreie nosso menu!',
+        menus: results
+      });
+    });
+  });
+
+  router.get('/reservations', function (req, res, next) {
+
+    reservations.render(req, res);
+
+  });
+
+  router.post('/reservations', function (req, res, next) {
+
+    if (!req.body.name) {
+      reservations.render(req, res, "Digite o nome");
+    } else if (!req.body.email) {
+      reservations.render(req, res, "Digite o email");
+    } else if (!req.body.people) {
+      reservations.render(req, res, "Selecione quantidade de pessoas");
+    } else if (!req.body.date) {
+      reservations.render(req, res, "Digite a data");
+    } else if (!req.body.time) {
+      reservations.render(req, res, "Digite a hora");
+    } else {
+
+      reservations.save(req.body).then(results => {
+
+        req.body = {};
+
+        reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+      }).catch(err => {
+
+        reservations.render(req, res, err.message);
+
+      })
+
+    }
+  });
+
+
+  router.get('/services', function (req, res, next) {
+    res.render('services', {
+      title: "Serviços - Restaurante Saboroso",
+      background: 'images/img_bg_1.jpg',
+      h1: 'É um prazer poder servir!'
+    });
   });
 });
 
